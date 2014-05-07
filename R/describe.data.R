ca1c6a146c13edbf68112b04cbe1ea2a275c05da
mike071187 <michael.gahan@gmail.com>
2014-05-06 20:24:27 -0500

    2nd Commit

--- R/describe.data.R
+++ R/describe.data.R
@@ -16,3 +16,23 @@
 #' @format A data frame with 122 rows and 35 variables
 #' @name functionalTraits
 NULL
+
+#' Dendrometer032014
+#' 
+#' A dataset containing Dendrometer Data from March 2014
+#' 
+#' \itemize{
+#'   \item Order
+#'   \item tag
+#'   \item plot
+#'   \item q20
+#'   \item p5
+#'   \item many more variables
+#'   ...
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 7084 rows and 16 variables
+#' @name Dendrometer032014
+NULL
