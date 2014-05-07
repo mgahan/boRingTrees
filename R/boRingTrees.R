9fb285b62aff2ec6e1ae0966c29181dda97a5188
mike071187 <michael.gahan@gmail.com>
2014-05-06 21:40:58 -0500

    number 4

---
+++ R/boRingTrees.R
@@ -0,0 +1,40 @@
+#' Functional Traits Overall
+#' 
+#' A dataset containing Functional Traits Overall
+#' 
+#' \itemize{
+#'   \item species
+#'   \item Chlorophyll
+#'   \item Toughness
+#'   \item Thickness
+#'   \item Many more variables
+#'   ...
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 122 rows and 35 variables
+#' @name functionalTraits
+#' @usage data(functionalTraits)
+NULL
+
+#' Dendrometer032014
+#' 
+#' A dataset containing Dendrometer Data from March 2014
+#' 
+#' \itemize{
+#'   \item Order
+#'   \item tag
+#'   \item plot
+#'   \item q20
+#'   \item p5
+#'   \item many more variables
+#'   ...
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @usage data(Dendrometer032014)
+#' @format A data frame with 7084 rows and 16 variables
+#' @name Dendrometer032014
+NULL
